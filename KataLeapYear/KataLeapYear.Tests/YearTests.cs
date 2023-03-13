676d6cd0e3f9e82213e6c5c2e280a9c55fccf34f
Victor Garcia <vgarcia@aidacanarias.com>
2023-03-13 13:11:13 +0100

    Refact: Add more test cases

--- KataLeapYear/KataLeapYear.Tests/YearTests.cs
+++ KataLeapYear/KataLeapYear.Tests/YearTests.cs
@@ -10,9 +10,11 @@ namespace KataLeapYear.Tests
         }
 
         [Theory]
+        [TestCase(1200)]
         [TestCase(1600)]
         [TestCase(2000)]
         [TestCase(2400)]
+        [TestCase(2800)]
         public void IsLeapYearWhenDivisibleBy400(int value)
         {
             var year = new Year(value);
@@ -21,9 +23,12 @@ namespace KataLeapYear.Tests
         }
 
         [Theory]
+        [TestCase(1992)]
         [TestCase(1996)]
         [TestCase(2004)]
         [TestCase(2012)]
+        [TestCase(2016)]
+        [TestCase(2020)]
         public void IsLeapYearWhenDivisibleBy4(int value)
         {
             var year = new Year(value);
@@ -31,15 +36,24 @@ namespace KataLeapYear.Tests
             Assert.True(year.IsLeap());
         }
 
-        [Test]
-        public void IsNotLeapYear2023()
+        [Theory]
+        [TestCase(2017)]
+        [TestCase(2018)]
+        [TestCase(2019)]
+        [TestCase(2021)]
+        [TestCase(2022)]
+        [TestCase(2023)]
+        public void IsNotLeapYearWhenItsNotDivisibleBy4(int value)
         {
-            var year = new Year(2023);
+            var year = new Year(value);
 
             Assert.False(year.IsLeap());
         }
         
         [Theory]
+        [TestCase(1100)]
+        [TestCase(1300)]
+        [TestCase(1500)]
         [TestCase(1700)]
         [TestCase(1800)]
         [TestCase(1900)]
