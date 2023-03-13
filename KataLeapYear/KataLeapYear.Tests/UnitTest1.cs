17101efd81de75ed799133e251d1b79403eef4d0
Victor Garcia <vgarcia@aidacanarias.com>
2023-03-13 12:42:13 +0100

    Initial files

---
+++ KataLeapYear/KataLeapYear.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace KataLeapYear.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
